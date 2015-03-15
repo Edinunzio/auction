442231604050f37442bfaf30480e77d5ac7e2a2f
Edinunzio <e.dinunzio@gmail.com>
2015-03-15 17:54:44 -0400

    Stubbing list view, stubbing bid price check

--- weBay/views.py
+++ weBay/views.py
@@ -5,14 +5,20 @@ from forms import BidForm
 
 def index(request):
     items = get_list_or_404(Item)
-    return HttpResponse(items)
+    return render(request, 'list.html', {'items': items})
+    #return HttpResponse(items)
 
 def item_detail(request, item_id):
     item = get_object_or_404(Item, pk=item_id)
+    current_price = item.current_price
     if request.method == 'POST':
         form = BidForm(request.POST)
         if form.is_valid():
-            return HttpResponse('bid accepted!')
+            if form.bid_price > current_price:
+                # update bid for item
+                return HttpResponse('bid accepted!')
+            else:
+                return HttpResponse('bid rejected. aim higher')
     else:
         form = BidForm()
         return render(request, 'detail.html', {'form': form, 'item': item})
